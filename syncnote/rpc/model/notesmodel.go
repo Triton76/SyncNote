26d5ebb15d5e25e078e63e1f98b4a18071fc7180
Triton76 <1573382476@qq.com>
2026-03-17 04:45:10 +0000

    fix: 转移syncnoteapi,rpc服务文件夹 feat: syncnoterpc mysql支持（未完善）

---
+++ syncnote/rpc/model/notesmodel.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ NotesModel = (*customNotesModel)(nil)
+
+type (
+	// NotesModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customNotesModel.
+	NotesModel interface {
+		notesModel
+	}
+
+	customNotesModel struct {
+		*defaultNotesModel
+	}
+)
+
+// NewNotesModel returns a model for the database table.
+func NewNotesModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) NotesModel {
+	return &customNotesModel{
+		defaultNotesModel: newNotesModel(conn, c, opts...),
+	}
+}
