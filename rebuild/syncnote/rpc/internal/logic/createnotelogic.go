df73e93b6cce7efbc0afd836cefed35299efd5b9
Triton76 <1573382476@qq.com>
2026-03-22 11:14:51 +0000

    nothing

--- rebuild/syncnote/rpc/internal/logic/createnotelogic.go
+++ rebuild/syncnote/rpc/internal/logic/createnotelogic.go
@@ -2,12 +2,14 @@ package logic
 
 import (
 	"context"
+	"database/sql"
 
 	"SyncNote/rebuild/common/model"
 	"SyncNote/rebuild/pkg/middleware"
 	"SyncNote/rebuild/syncnote/rpc/internal/svc"
 	"SyncNote/rebuild/syncnote/rpc/pb/syncnoterpc"
 
+	"github.com/google/uuid"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -28,12 +30,19 @@ func NewCreateNoteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Create
 func (l *CreateNoteLogic) CreateNote(in *syncnoterpc.CreateNoteRequest) (*syncnoterpc.CreateNoteResponse, error) {
 	// 创建，插入数据库，设置owner为当前用户。
 	userId, err := middleware.GetUserFromContext(l.ctx)
-		if err != nil {
+	if err != nil {
 		return nil, err
 	}
+	content := sql.NullString{String: in.content, Valid: in.content != ""}
 	res, err := l.svcCtx.NoteModel.Insert(l.ctx, &model.Note{
-		NoteId: uuid.NewString(),
-		OwnerId: 
+		NoteId:  uuid.NewString(),
+		OwnerId: userId,
+		Title:   in.Title,
+		Content: content,
+		Version: 1,
 	})
+	if err != nil {
+		return nil, err
+	}
 	return &syncnoterpc.CreateNoteResponse{}, nil
 }
